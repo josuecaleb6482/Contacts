95e4d360140ebc537870164f5cd7f960141d3be8
Josue Florez <josue.florez@utp.ac.pa>
2021-08-09 21:33:43 -0500

    Commite

--- Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,77 +13,77 @@ namespace Blazor.Contacts.Wasm.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 1 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 2 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 3 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 4 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 5 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 6 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 7 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 8 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 9 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Blazor.Contacts.Wasm.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 10 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Blazor.Contacts.Wasm.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/_Imports.razor"
+#line 11 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using MudBlazor;
 
 #line default
@@ -97,7 +97,7 @@ using MudBlazor;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "/home/josue/Documentos/proyectos/Contactos/Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/Shared/SurveyPrompt.razor"
+#line 12 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
