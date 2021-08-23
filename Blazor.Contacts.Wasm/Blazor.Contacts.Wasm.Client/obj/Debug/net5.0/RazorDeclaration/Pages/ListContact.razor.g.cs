41d7b103847ca5bd2d60078236c6c2a3cda28354
Josue Florez <josue.florez@utp.ac.pa>
2021-08-22 21:35:46 -0500

    Add: MudBlazor, FomrDialog, Method EditContact, DeleteContact Edit: ListContacts Delete: CreateContatc

--- Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/obj/Debug/net5.0/RazorDeclaration/Pages/ListContact.razor.g.cs
+++ Blazor.Contacts.Wasm/Blazor.Contacts.Wasm.Client/obj/Debug/net5.0/RazorDeclaration/Pages/ListContact.razor.g.cs
@@ -12,84 +12,91 @@ namespace Blazor.Contacts.Wasm.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 1 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 2 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 3 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 4 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 5 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 6 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 7 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 8 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 9 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Blazor.Contacts.Wasm.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 10 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using Blazor.Contacts.Wasm.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+#line 11 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\Pages\ListContact.razor"
+#line 12 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\_Imports.razor"
+using Shared.Dialog;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\JFlorez\source\repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\Pages\ListContact.razor"
 using System.Collections.Generic;
 
 #line default
@@ -103,25 +110,8 @@ using System.Collections.Generic;
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 42 "C:\Users\JFlorez\Source\Repos\Contacts\Blazor.Contacts.Wasm\Blazor.Contacts.Wasm.Client\Pages\ListContact.razor"
-      
-    public IEnumerable<Contact> contacts;
-
-    protected async override Task OnInitializedAsync()
-    {
-        contacts = await ContactService.GetAll();
-    }
-    
-    public void Edit(int id)
-    {
-        NavigationManager.NavigateTo($"./contactdetails/{id}");
-    }
-
-
-#line default
-#line hidden
-#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private ISnackbar Snackbar { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IDialogService DialogServices { get; set; }
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private NavigationManager NavigationManager { get; set; }
         [global::Microsoft.AspNetCore.Components.InjectAttribute] private IContactService ContactService { get; set; }
     }
